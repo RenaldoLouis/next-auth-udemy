c89c0401ac501aa9e6746d3deeffec8e6c16ba6f
RenaldoLouis <renald@10xers.co>
2024-02-24 20:18:39 +0700

    add register and login UI

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth/next";
-
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const authHandler = NextAuth({
@@ -12,15 +11,45 @@ const authHandler = NextAuth({
             },
             async authorize(credentials) {
                 try {
-                    return null;
+                    const response = await
+                        fetch(`${process.env.NEXTAUTH_URL}/api/login`, {
+                            method: 'POST',
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                email: credentials?.email,
+                                password: credentials?.password
+                            })
+                        })
+
+                    const json = await response.json();
+
+                    if (response.status === 200) {
+                        return json.result;
+                    }
+                    else {
+                        throw (JSON.stringify(json));
+                    }
                 }
                 catch (e) {
                     throw new Error(e);
                 }
             }
         })
-    ]
-
+    ],
+    pages: {
+        signIn: "/login"
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            return ({ ...token, ...user });
+        },
+        async session({ session, token }) {
+            session.user = token;
+            return session;
+        }
+    }
 })
 
-export { authHandler as GET, authHandler as POST }
+export { authHandler as GET, authHandler as POST };
