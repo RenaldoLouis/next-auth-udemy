23900074cb65737f7f54f78673d029fdec451a23
RenaldoLouis <renald@10xers.co>
2024-02-25 22:22:07 +0700

    basic authorization done

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,22 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css'
+import { Inter } from 'next/font/google'
+import AuthProvider from '@/providers/AuthProvider'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Next Auth Udemy",
-  description: "NAU",
-};
+  title: 'Next Auth Udemy',
+  description: 'NAU',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
-  );
+  )
 }
--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
+
+import LogoutButton from "@/components/LogoutButton"
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      page
+    <main
+      className="bg-black flex flex-col justify-center items-center space-y-8 h-screen"
+    >
+      <h1
+        className="text-white text-center text-2xl font-bold uppercase"
+      >
+        You have logged in!
+      </h1>
+      <img
+        src="/treasure.png"
+        alt="treasure chest illustration"
+        className="w-1/4 object-contain"
+      />
+      <LogoutButton />
     </main>
-  );
+  )
 }
